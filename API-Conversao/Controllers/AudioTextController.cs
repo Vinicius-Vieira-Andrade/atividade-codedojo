559c96657488d588e35165ae1d87807e579172d8
Vinícius Vieira De Andrade Correia <53685550837@info.local>
2024-04-19 15:04:49 -0300

    desenvolvimento da atividade code dojo

---
+++ API-Conversao/Controllers/AudioTextController.cs
@@ -0,0 +1,73 @@
+﻿using API_Conversao.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.CognitiveServices.Speech;
+
+namespace API_Conversao.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AudioTextController : ControllerBase
+    {
+        public IFormFile? file { get; set; }
+
+        [HttpPost("TextoPraVoz")]
+        public async Task<IActionResult> ConvertTextToAudio(string text)
+        {
+            //configuracoes do servico cognitivo da azure
+            var speechConfig = SpeechConfig.FromSubscription("63fe14ea7e474db5990b1a98825b4658", "eastus");
+
+            using (var speech = new SpeechSynthesizer(speechConfig))
+            {
+                var result = await speech.SpeakTextAsync(text);
+                if (result.Reason == ResultReason.SynthesizingAudioCompleted)
+                {
+                    return Ok("Texto convertido com sucesso" + result);
+                }
+
+                return BadRequest("deu erro" + result);
+            }
+        }
+
+        //[HttpGet]
+        //public async Task<IActionResult> ConvertAudioText([FromForm] IFormFile? file)
+        //{
+        //    return Ok();
+        //}
+
+
+        [HttpPost]
+        public async Task<IActionResult> ConvertAudioText([FromForm] IFormFile? file)
+        {
+            var containerName = "audio-api";
+
+            var stringConexao = "DefaultEndpointsProtocol=https;AccountName=vitahubg10;AccountKey=0f4F0m+JXwms9KPYeHtqmQ1m5ggzPOieFXDt7VfWqcUV1qDiviHjtesLPYi0jBJ7xDOiORGa4rHp+ASto9sJxw==;EndpointSuffix=core.windows.net";
+
+            var audio = await FileAudio.UploadAudioBlobAsync(file!, stringConexao, containerName);
+
+            return Ok(audio);
+             
+                //try
+                //{
+                //    var speechConfig = SpeechConfig.FromSubscription("63fe14ea7e474db5990b1a98825b4658", "eastus");
+                //    speechConfig.SpeechRecognitionLanguage = "pt-BR";
+                //    using var audioConfig = AudioConfig.FromWavFileInput(audio);
+                //    using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+                //    var result = await speechRecognizer.RecognizeOnceAsync();
+
+                //    return Ok(result);
+                //}
+                //catch (Exception erro)
+                //{
+                //    return BadRequest(erro.Message);
+                //}
+            
+
+            //return BadRequest("deu erro");
+
+
+        }
+    }
+}
---
+++ API-Conversao/Utils/FileAudio.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Storage.Blobs;
+
+namespace API_Conversao.Utils
+{
+    public class FileAudio
+    {
+        public static async Task<string> UploadAudioBlobAsync(IFormFile arquivo, string stringConexao, string nomeContainer)
+        {
+            try
+            {
+                //VERIFICA SE EXISTE UM ARQUIVO
+                if (arquivo != null)
+                {
+                    //GERA UM NOME UNICO + EXTENSAO DO ARQUIVO
+                    var blobName = Guid.NewGuid().ToString().Replace("-", "") + Path.GetExtension(arquivo.FileName);
+
+                    //CRIAR UMA INSTANCIA DO CLIENT BLOBSERVICE E PASSA A STRING DE CONEXAO
+                    var blobServiceClient = new BlobServiceClient(stringConexao);
+
+                    //OBTEM UM CONTAINER CLIENT USANDO O NOME DO CONTAINER DO BLOB
+                    var blobContainerClient = blobServiceClient.GetBlobContainerClient(nomeContainer);
+
+                    //OBTEM UM BLOB CLIENT USANDO O NAME
+                    var blobClient = blobContainerClient.GetBlobClient(blobName);
+
+                    //ABRE O FLUXO DE ENTRADA DO ARQUIVO(FOTO)
+                    using (var stream = arquivo.OpenReadStream())
+                    {
+                        //CARREGA O ARQUIVO(FOTO) PARA O BLOB STORAGE DE FORMA ASSINCRONA 
+                        await blobClient.UploadAsync(stream, true);
+                    }
+                    //RETORNA A URI DO BLOB COMO STRING
+                    return blobClient.Uri.ToString();
+                }
+                return null;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
